4fdf7bad8a3e347ee89f53ead3d46a3223543568
shenxianovo <shenxianovo@gmail.com>
2025-05-12 19:03:43 +0800

    Project initialization.

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+    // 站点级选项
+    title: "WHUDAYS", // 网站标题
+    description: "武汉大学动漫协会工作文档", // 网站描述
+
+    themeConfig: {
+        // 主题级选项
+        nav: [
+            { text: '关于', link: '/about' },
+            {
+                text: '文档',
+                items: [
+                    { text: "首页", link: "/" },
+                    { text: "1", link: "/bigFrontEnd/html/" },
+                    { text: "2", link: "/bigFrontEnd/css/" },
+                    { text: "3", link: "/bigFrontEnd/js/" },
+                ]
+            }
+        ]
+    }
+};
