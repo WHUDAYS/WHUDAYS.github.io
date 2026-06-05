d173cc46e6f5d2cf7eaf11363f916df636ed141e
shenxianovo <shenxianovo@outlook.com>
2026-06-06 00:28:59 +0800

    chore: 站点元数据域名统一为 whudays.org
    sitemap.hostname 与 og:url 原为 whudays.github.io，与正式域名
    （CNAME: whudays.org）不一致，导致 sitemap 与社交分享卡片指向旧地址。
    统一为 whudays.org。

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -24,7 +24,7 @@ export default defineConfig({
 	title: "武汉大学动漫协会-WHUDAYS", // 网站标题
 	description: "武汉大学动漫协会", // 网站描述
 	sitemap: {
-		hostname: "https://whudays.github.io/", // 网站域名
+		hostname: "https://whudays.org/", // 网站域名
 	},
 	head: [
 		['meta', { name: 'keywords', content: '武汉大学动漫协会, 武大漫协, WHUDAYS, ACGN, 动漫社团, 武大动漫社' }],
@@ -35,7 +35,7 @@ export default defineConfig({
 		['meta', { property: 'og:title', content: '武汉大学动漫协会-WHUDAYS' }],
 		['meta', { property: 'og:description', content: '武汉大学动漫协会官方网站，记录社团历史与活动，快来看看吧（！）\\^o^/' }],
 		['meta', { property: 'og:image', content: '/WHUDAYS.png' }],
-		['meta', { property: 'og:url', content: 'https://whudays.github.io/' }],
+		['meta', { property: 'og:url', content: 'https://whudays.org/' }],
 
 		// Google Analytics
 		['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-LQLQ2CEQ64' }],
